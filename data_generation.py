b23ecb2f4426198e50609f943347bbcd8a528f4a
Nathan Villiger <nvillig2@uoregon.edu>
2023-03-01 09:24:44 -0800

    sort inds by location w/ MDS

--- data_generation.py
+++ data_generation.py
@@ -10,6 +10,7 @@ import warnings
 from attrs import define,field
 from read_input import *
 # from process_input import *
+from sklearn.manifold import MDS
 
 @define
 class DataGenerator(tf.keras.utils.Sequence):
@@ -234,8 +235,21 @@ class DataGenerator(tf.keras.utils.Sequence):
                 if d > sampling_width:
                     sampling_width = float(d)
 
+        # MDS to reorder samples
+        mds = MDS(random_state=seed, n_components=1)
+        locs_mds = mds.fit_transform(locs).flatten()
+        new_indices = np.argsort(locs_mds)
+
+        # alternatively, order by x axis
+        # new_indices = np.argsort(locs[:,0])
+
         # grab genos
         geno_mat0 = ts.genotype_matrix()
+        interleaved_indices = []
+        for i in new_indices:
+            interleaved_indices.append(i*2)
+            interleaved_indices.append(i*2+1)
+        geno_mat0 = geno_mat0[:,interleaved_indices]
 
         # change 0,1 encoding to major/minor allele
         if self.polarize == 2:
