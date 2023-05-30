299e51949cf5f302135f8bc057940b72739d5a38
Nathan Villiger <nvillig2@uoregon.edu>
2023-05-30 16:27:13 -0700

    update

--- train_network.sh
+++ train_network.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=kern
-#SBATCH --job-name=prod4
+#SBATCH --job-name=prod3s
 #SBATCH --output=%x.txt
 #SBATCH --error=%x.txt
-#SBATCH --time=14-00:00:00
+#SBATCH --time=30-00:00:00
 #SBATCH --mem=20G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -19,7 +19,8 @@ conda activate jupyterlab-tf-plus-20220915
 
 D0=training_data
 NUM=100
-OUTNAME=production_N${NUM}_trip_lr4
+LRPOW=3
+OUTNAME=production_mean_N${NUM}_lr${LRPOW}
 python disperseNN.py \
   --train \
   --gpu_index -1 \
@@ -38,8 +39,8 @@ python disperseNN.py \
   --batch_size 50 \
   --threads ${SLURM_CPUS_PER_TASK} \
   --max_epochs 1000 \
-  --learning_rate 1e-4 \
+  --learning_rate 1e-${LRPOW} \
   --keras_verbose 2 \
   --seed 12345 \
-  --out ${D0}/${OUTNAME} 
-  # --MDS_sorting
+  --out ${D0}/${OUTNAME} \
+  --MDS_sorting
